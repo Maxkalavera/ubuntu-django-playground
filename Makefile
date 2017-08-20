44e1d5c4c2bb0180fdf22137be371666df586fbd
max H <maxhernandezcastillo@gmail.com>
2017-08-19 20:26:17 -0500

    Added virtual env funcionality and an Vagrant file.

--- Makefile
+++ Makefile
@@ -1,5 +1,20 @@
 PROJECT_ROOT=.
 INSTALL_SCRIPT_PATH=$(PROJECT_ROOT)/install/install.sh
 
+print-current-path:
+	@$(INSTALL_SCRIPT_PATH) print-current-path
+
+print-project-name:
+	@$(INSTALL_SCRIPT_PATH) print-project-name
+
+create-django-project:
+	@$(INSTALL_SCRIPT_PATH) create-django-project
+
+set-up-enviroment-vagrant:
+	@$(INSTALL_SCRIPT_PATH) set-up-enviroment-vagrant
+
+set-up-enviroment-virtualenv:
+	@$(INSTALL_SCRIPT_PATH) set-up-enviroment-virtualenv
+
 hello-world:
 	@$(INSTALL_SCRIPT_PATH) hello-world
--- install/install.sh
+++ install/install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-PROJECT_ROOT=.
-PIP_PROJECT_NAME=venv
-INSTALL_FOLDER=$PROJECT_ROOT/install/
 
-PROJECT_NAME=blog
+PROJECT_ROOT=.
+# some variables are taken of this env file, for example PROJECT_NAME  can
+# be found here.
+ENV_PATH=$PROJECT_ROOT/conf/.env
+source $ENV_PATH
+
+INSTALL_FOLDER=$PROJECT_ROOT/install/
 DJANGO_ROOT=./$PROJECT_NAME/
 
 ###############################################################################
 # Utils
 ###############################################################################
 
-function create-django-project {
-  django-admin.py $PROJECT_NAME
-}
-
 function start-env {
-  source ./$PIP_PROJECT_NAME/bin/activate
+  source ./$PIP_FOLDER/bin/activate
 }
 
 function finish-env {
@@ -30,6 +29,14 @@ function print {
   echo -e '\n'$1
 }
 
+function print-current-path {
+  print $PWD
+}
+
+function print-project-name {
+  print $PROJECT_NAME
+}
+
 ###############################################################################
 # Clean functions
 ###############################################################################
@@ -38,7 +45,7 @@ function clean-venv {
   print 'Desinstalando venv'
   sudo pip3 uninstall virtualenv
   print 'Eliminando virtualenv directory'
-  rm -R ./$PIP_PROJECT_NAME/
+  rm -R ./$PIP_FOLDER/
   rm venv
 }
 
@@ -66,16 +73,14 @@ function install-virtualenv {
   print 'Instaling virtualenv'
   pip3 install virtualenv
   print 'Starting the venv project'
-  virtualenv $PIP_PROJECT_NAME --no-site-packages
+  virtualenv $PIP_FOLDER --no-site-packages
   print 'Creating symlink venv to virtualenv/bin/activate'
-  ln -s $PROJECT_ROOT/$PIP_PROJECT_NAME/bin/activate $PROJECT_ROOT/venv
+  ln -s $PROJECT_ROOT/$PIP_FOLDER/bin/activate $PROJECT_ROOT/env
 }
 
 function install-pip-requirements {
-  start-env
   print 'Installing pip requirements'
   pip install -r $INSTALL_FOLDER/requirements.txt
-  finish-env
 }
 
 function install-pip-requirements-in-virtualenv {
@@ -85,12 +90,31 @@ function install-pip-requirements-in-virtualenv {
   finish-env
 }
 
-function all() {
+function install-vagrant-with-virtualbox {
+  # vagrant==1:1.9.7
+  # virtualbox==5.0.40
+  sudo apt-get install vagrant=1:1.9.7 virtualbox=5.0.40*
+}
+
+function create-django-project {
+  django-admin.py $PROJECT_NAME
+}
+
+function set-up-enviroment-vagrant {
+  before-install
+  install-python
+  install-vagrant-with-virtualbox
+}
+
+function set-up-enviroment-virtualenv {
   before-install
   install-python
   install-virtualenv
-  #install-pip-requirements
   install-pip-requirements-in-virtualenv
+}
+
+function install-dependencies {
+  install-pip-requirements
 }
 
 # Call function from arguments
